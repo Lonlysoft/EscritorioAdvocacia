42e09f0cb85c7c1e18a0f8463c6a0774570f42c9
LonlySoft <120424053+Lonlysoft@users.noreply.github.com>
2025-07-04 12:12:04 -0300

    Update PessoaController.java

--- src/controllers/PessoaController.java
+++ src/controllers/PessoaController.java
@@ -6,77 +6,80 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import domain.Tribunal;
-import dtos.TribunalDto;
-import exceptions.TribunalException;
+import domain.Advogado;
+import domain.PessoaFisica;
+import domain.PessoaJuridica;
+import dtos.PessoaFisicaDto;
+import exceptions.CpfException;
+import exceptions.EmailException;
+import exceptions.PessoaException;
+import shared.Cpf;
+import shared.Email;
 
 public class PessoaController implements Serializable {
 
 	private static final long serialVersionUID = -4125026648925415158L;
-	
+
 	private Map<String, PessoaFisica> pessoasFisicas;
 	private Map<String, PessoaJuridica> pessoasJuridicas;
 	private Map<String, Advogado> advogados;
 
 	protected PessoaController() {
-		pessoas = new TreeMap<>();
+		pessoasFisicas = new TreeMap<>();
+		pessoasJuridicas = new TreeMap<>();
+		advogados = new TreeMap<>();
 	}
 
-	public void createPessoaFisica(PessoaFisicaDto pfDto) throws PessoaException {
+	public void createPessoaFisica(PessoaFisicaDto pfDto) throws PessoaException, CpfException, EmailException {
 
-		if (pessoas.get(tribunalDto.getSigla()) != null) {
-			throw new TribunalException("Já existe tribunal cadastrado para a sigla: " + tribunalDto.getSigla());
+		if (pessoasFisicas.get(pfDto.getCpf()) != null) {
+			throw new PessoaException("Já existe pessoa cadastrado para o CPF: " + pfDto.getCpf());
 		}
 
-		PessoaFisica pf = PessoaFisica(pfDto.getNome(), pfDto.getTelefone(), new Email(pfDto.getEmail()), new Cpf(pfDto.getCpf()));
+		PessoaFisica pf = new PessoaFisica(pfDto.getNome(), new Email(pfDto.getEmail()), pfDto.getTelefone(), new Cpf(pfDto.getCpf()));
 
-		pessoasFisicas.put(pf.getRegistroRF(), pf
-		);
+		pessoasFisicas.put(pf.getCpf(), pf);
 		
 		MainController.save();
 	}
-	
 
-	public void updatePessoaFisica(PessoaFisicaDto pfDto) throws PessoaFisicaException {
+	public void updatePessoaFisica(PessoaFisicaDto pfDto) throws PessoaException, EmailException {
 
 		PessoaFisica pf = pessoasFisicas.get(pfDto.getCpf());
 
-		if (tribunal == null)
-			throw new PessoaException("Não tem tribunal cadastrado para a sigla: " + tribunalDto.getSigla());
+		if (pf == null)
+			throw new PessoaException("Não tem pessoa cadastrado para o CPF: " + pfDto.getCpf());
 
-		tribunal.setNome(tribunalDto.getNome());
-		tribunal.setSecao(tribunalDto.getSecao());
-		
+		pf.setNome(pfDto.getNome());
+		pf.setEmail(pfDto.getEmail());
+
 		MainController.save();
 	}
 
-	public TribunalDto getPessoaFisica(String sigla) throws TribunalException {
+	public PessoaFisicaDto getPessoaFisica(String cpf) throws PessoaException{
 
-		Tribunal tribunal = pessoas.get(sigla);
+		PessoaFisica pessoa = pessoasFisicas.get(cpf);
 
-		if (tribunal == null)
-			throw new TribunalException("Não tem tribunal cadastrado para a sigla: " + sigla);
+		if (pessoa == null)
+			throw new PessoaException("Não tem pessoa cadastrado para o CPF: " + cpf);
 
-		TribunalDto tribunalDto = new TribunalDto(tribunal.getSigla(), tribunal.getNome(), tribunal.getSecao());
+		PessoaFisicaDto pessoaDto = new PessoaFisicaDto(pessoa.getNome(), pessoa.getEmail(), pessoa.getTelefone(), pessoa.getCpf());
 
-		return tribunalDto;
+		return pessoaDto;
 	}
 
-	public ListPessoasFisicas<TribunalDto> getpessoas() {
+	public List<PessoaFisicaDto> getpessoas() {
 
 		List<PessoaFisicaDto> lista = new ArrayList<>();
 
 		PessoaFisicaDto pfDto;
 
 		for (PessoaFisica pf : pessoasFisicas.values()) {
-			pfDto = new PessoaFisicaDto();
+			pfDto = new PessoaFisicaDto(pf.getNome(), pf.getEmail(), pf.getTelefone(), pf.getCadastroRF());
 			lista.add(pfDto);
 		}
 
 		return lista;
 	}
-	
-	public PessoaFisicaDto findPreposto(cpf){
-		return pessoasJuridicas.get(cpf);
-	}
+
 }
